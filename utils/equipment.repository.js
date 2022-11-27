3e531e934245a9334b7b3597c16d0c7468fb12a2
JulienS78 <75180034+JulienS78@users.noreply.github.com>
2022-11-27 16:49:22 +0100

    Update equipment.repository.js

--- utils/equipment.repository.js
+++ utils/equipment.repository.js
@@ -65,11 +65,12 @@ module.exports = {
         }
     },
 
-    async addOneEquipement(equipement_ID, name, type, condition, available, purchase_date, storage_place, renting_rate, bail_rate){ 
+    async addOneEquipement(name, type, condition, available, purchase_date, storage_place, renting_rate, bail_rate){ 
         try {
             let conn = await pool.getConnection();
-            let sql = "INSERT INTO equipement (equipement_ID, name, type, condition, available, purchase_date, storage_place, renting_rate, bail_rate) VALUES (NULL, ?) ";
-            const [okPacket, fields] = await conn.execute(sql, [equipement_ID, name, type, condition, available, purchase_date, storage_place, renting_rate, bail_rate]);
+            let sql = "INSERT INTO equipement (equipement_ID, name, type, condition, available, purchase_date, storage_place, renting_rate, bail_rate) VALUES (NULL, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+            const [okPacket, fields] = await conn.execute(sql, 
+                        [name, type, condition, available, purchase_date, storage_place, renting_rate, bail_rate]);
             conn.release();
             console.log("INSERT "+JSON.stringify(okPacket));
             return okPacket.insertId;
@@ -83,7 +84,7 @@ module.exports = {
     async editOneEquipement(equipement_ID, name, type, condition, available, purchase_date, storage_place, renting_rate, bail_rate){ 
         try {
             let conn = await pool.getConnection();
-            let sql = "UPDATE cars SET car_brand=?, car_name=?, car_baseprice=?, car_isFancy=?, car_realPrice=? WHERE car_id=? "; // TODO: named parameters? :something
+            let sql = "UPDATE equipement SET name=?, type=?, condition=?, available=?, purchase_date=?, storage_place=?, renting_rate=?, bail_rate=?, WHERE equipement_ID=?"; // TODO: named parameters? :something
             const [okPacket, fields] = await conn.execute(sql, 
                         [name, type, condition, available, purchase_date, storage_place, renting_rate, bail_rate, equipement_ID]);
             conn.release();
@@ -97,82 +98,5 @@ module.exports = {
     }
 };
 
-
-
-
-
-class equipment{
-
-    constructor(){}
-    
-    constructor(name){
-        this.name = name;
-    }
-
-    constructor(name, type, condition){
-        this.name = name;
-        this.type = type;
-        this.condition = condition;
-    }
-    constructor(name, type, condition, available, storage_place, purchase_date, renting_rate, bail_rate){
-        this.name = name;
-        this.type = type;
-        this.condition = condition;
-        this.available = available;
-        this.storage_place = storage_place;
-        this.purchase_date = purchase_date;
-        this.renting_rate = renting_rate;
-        this.bail_rate = bail_rate;
-    }
-
-    get getName() {
-        return this.name;
-    }
-    get getType() {
-        return this.type;
-    }
-    get getCondition() {
-        return this.condition;
-    }
-    get getAvailable() {
-        return this.available;
-    }
-    get getStorage_place() {
-        return this.storage_place;
-    }
-    get getPurchase_date() {
-        return this.purchase_date;
-    }
-    get getRenting_rate() {
-        return this.renting_rate;
-    }
-    get getBail_rate() {
-        return this.bail_rate;
-    }
-    set setName(newName) {
-        this.name = newName;
-    }
-    set setType(newType) {
-        this.type = newType;
-    }
-    set setCondition(newCondition) {
-        this.condition = newCondition;
-    }
-    set setAvailable(newAvailable) {
-        this.available = newAvailable;
-    }
-    set setPurchase_date(newPurchase_date) {
-        this.purchase_date = newPurchase_date;
-    }
-    set setStorage_place(newStorage_place) {
-        this.storage_place = newStorage_place;
-    }
-    set setRenting_rate(newRenting_rate) {
-        this.renting_rate = newRenting_rate;
-    }
-    set setBail_rate(newBail_rate) {
-        this.bail_rate = newBail_rate;
-    }
-}
 
   
