f97346761305996774b2b458d99e9a57a00f4aa1
Cyb0nix <116296259+Cyb0nix@users.noreply.github.com>
2022-11-30 00:48:21 +0100

    update client.routes

--- Code/controllers/client.routes.js
+++ Code/controllers/client.routes.js
@@ -1,64 +1,68 @@
-// controllers/cars.route.js
+// controllers/client.route.js
 const express = require('express');
 const router = express.Router();
-const clientRepo = require('../utils/clients.repository');
+const clientRepo = require('../utils/equipment.repository');
 
 router.get('/', clientRootAction);
 router.get('/list', clientListAction);
-// router.get('/show/:carId', carShowAction);
-// router.get('/del/:carId', carDelAction);
-// router.get('/edit/:carId', carEditAction);
-// router.post('/update/:carId', carUpdateAction);
+router.get('/show/:clientId', clientShowAction);
+router.get('/del/:clientId', clientDelAction);
+router.get('/edit/:clientId', clientEditAction);
+router.post('/update/:clientId', clientUpdateAction);
 
-// http://localhost:9000/client
+
 function clientRootAction(request, response) {
-    console.log('root');
     //response.send("ROOT ACTION");
     response.redirect("/client/list");
 }
 async function clientListAction(request, response) {
     // response.send("LIST ACTION");
-    var client = await clientRepo.getAllClient();
-    console.log(client);
-    response.send(client);
+    var client = await clientRepo.getAllclient();
+    // console.log(client);
+
+    
+    response.render(client);
 }
-// async function carShowAction(request, response) {
-//     // response.send("SHOW ACTION");
-//     var oneCar = await carRepo.getOneCar(request.params.carId);
-//     response.render("cars_show", { "oneCar": oneCar });
-// }
-// async function carEditAction(request, response) {
-//     // response.send("EDIT ACTION");
-//     var brands = await carRepo.getAllBrands();
-//     var carId = request.params.carId;
-//     if (carId!=="0")
-//         var car = await carRepo.getOneCar(carId);
-//     else
-//         var car = carRepo.getBlankCar();
-//     response.render("cars_edit", { "oneCar": car, "brands": brands });
-// }
-// async function carDelAction(request, response) {
-//     // response.send("DEL ACTION");
-//     // TODO: remove extras for car, unless the car cannot be removed!!!
-//     var numRows = await carRepo.delOneCar(request.params.carId);
-//     request.session.flashMessage = "ROWS DELETED: "+numRows;
-//     response.redirect("/cars/list");
-// }
-// async function carUpdateAction(request, response) {
-//     // response.send("UPDATE ACTION");
-//     var carId = request.params.carId;
-//     if (carId==="0") carId = await carRepo.addOneCar(request.body.car_brand);
+async function clientShowAction(request, response) {
+    // response.send("SHOW ACTION");
+    var oneclient = await clientRepo.getOneclient(request.params.clientId);
+    response.send(oneclient);
+    response.render(oneclient);
+}
+async function clientEditAction(request, response) {
+    // response.send("EDIT ACTION");
+    var brands = await clientRepo.getAllclient();
+    var clientId = request.params.clientId;
+    if (clientId!== null)
+        var client = await clientRepo.getOneclient(clientId);
+    else
+        var client = clientRepo.getBlankclient();
+    response.send(client);
+    response.render(client);
+}
+async function clientDelAction(request, response) {
+    // response.send("DEL ACTION");
+    // TODO: remove extras for car, unless the car cannot be removed!!!
+    var numRows = await clientRepo.delOneclient(request.params.clientId);
+    request.session.flashMessage = "ROWS DELETED: "+numRows;
 
-//     var isFancy = request.body.car_isFancy === undefined ? 0 : 1; 
-//     var numRows = await carRepo.editOneCar(carId, 
-//         request.body.car_brand, 
-//         request.body.car_name, 
-//         request.body.car_baseprice, 
-//         isFancy, 
-//         request.body.car_realPrice);
+}
 
-//     request.session.flashMessage = "ROWS UPDATED: "+numRows;
-//     response.redirect("/cars/list");
-// }
+async function clientUpdateAction(request, response) {
+    // response.send("UPDATE ACTION");
+    var clientId = request.params.clientId;
+    if (clientId===null) {
+        clientId = await clientRepo.addOneclient(request.body.name, request.body.number, request.body.email);
+        response.send(clientId);
+    }else{
+        var numRows = await clientRepo.editOneclient(clientId, 
+            request.body.name, 
+            request.body.number,  
+            request.body.email);
+    
+        request.session.flashMessage = "ROWS UPDATED: "+numRows;
+    }
+    
+}
 
 module.exports = router;
