378fcd7b36fe06e99e357610f5e30bcced8886f1
Cyb0nix <116296259+Cyb0nix@users.noreply.github.com>
2022-11-30 16:08:59 +0100

    update project.routes

--- Code/controllers/project.routes.js
+++ Code/controllers/project.routes.js
@@ -1,7 +1,7 @@
 // controllers/project.route.js
 const express = require('express');
 const router = express.Router();
-const carRepo = require('../utils/project.repository');
+const projectRepo = require('../utils/project.repository');
 
 router.get('/', projectRootAction);
 router.get('/list', projectListAction);
@@ -22,12 +22,14 @@ async function projectListAction(request, response) {
     var flashMessage = request.session.flashMessage; // express-flash ...
     request.session.flashMessage = "";
     
-    response.render("project_list", { "project": project, "flashMessage": flashMessage });
+    response.render(project);
+    response.send(project);
 }
 async function projectShowAction(request, response) {
     // response.send("SHOW ACTION");
     var oneproject = await projectRepo.getOneproject(request.params.Project_ID);
-    response.render("project_show", { "oneProject": oneProject });
+    response.render(oneproject);
+    response.send(oneproject)
 }
 async function projectEditAction(request, response) {
     // response.send("EDIT ACTION");
@@ -37,32 +39,33 @@ async function projectEditAction(request, response) {
         var car = await projectRepo.getOneProject(Project_ID);
     else
         var project = projectRepo.getBlankProject();
-    response.render("project_edit", { "oneproject": project, "type": type });
+    response.render(project);
+    response.send(project);
 }
+
 async function projectDelAction(request, response) {
-    // response.send("DEL ACTION");
-    // TODO: remove extras for car, unless the car cannot be removed!!!
     var numRows = await projectRepo.delOneProject(request.params.Project_ID);
     request.session.flashMessage = "ROWS DELETED: "+numRows;
-    response.redirect("/project/list");
 }
+
 async function projectUpdateAction(request, response) {
-    // response.send("UPDATE ACTION");
     var Project_ID = request.params.Project_ID;
-    if (Project_ID==="0") Project_ID = await projectRepo.addOneProject(request.body.type);
-
-    var Is_Paid = request.body.Is_Paid === undefined ? 0 : 1; 
-    var numRows = await projectRepo.editOneProject(Project_ID, 
-        request.body.type,  
-        Is_Paid, 
-        request.body.starting_date,
-        request.body.ending_date,
-        request.body.storage_place,
-        request.body.Benefits,
-        request.body.state);
-
-    request.session.flashMessage = "ROWS UPDATED: "+numRows;
-    response.redirect("/project/list");
+    if (Project_ID==="0"){
+        Project_ID = await projectRepo.addOneProject(request.body.type);
+    }else{
+        var Is_Paid = request.body.Is_Paid === undefined ? 0 : 1;
+        var numRows = await projectRepo.editOneProject(Project_ID, 
+            request.body.type,  
+            Is_Paid, 
+            request.body.starting_date,
+            request.body.ending_date,
+            request.body.storage_place,
+            request.body.Benefits,
+            request.body.state);
+    
+        request.session.flashMessage = "ROWS UPDATED: "+numRows;
+    }
+    
 }
 
 module.exports = router;
