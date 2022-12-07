fad791bb8e075195189954d1b68b2d4d845708b1
Cyb0nix <116296259+Cyb0nix@users.noreply.github.com>
2022-12-07 19:25:45 +0100

    Added CORS

--- Code/server.js
+++ Code/server.js
@@ -1,28 +1,19 @@
-/*
-cd WEBAPP
-mkdir controllers models views
-npm init -y
-npm install mariadb express express-session body-parser passport dotenv ejs mysql2
->> https://www.npmjs.com/package/body-parser
-edit/copy files, then ... node server.js 
-*/
 
 const dotenv = require('dotenv');
 dotenv.config();
 
 const express = require('express');
+var cors = require('cors')
 const app = express();
-app.set("view engine", "ejs");
-app.set("views", "views");
-app.listen(process.env.WEB_PORT, '0.0.0.0',
-    function() { console.log("Listening on "+process.env.WEB_PORT); }
-);
+// app.use(cors())
 
-app.get('/', (request, response) => { // 'GET' as a HTTP VERB, not as a 'getter'!
-    let clientIp = request.ip;
-    response.send(`Hello, dear ${clientIp}. I am a nodejs website...`);
-    response.end(); // optional
-});
+app.listen(process.env.WEB_PORT, function () {
+    console.log('CORS-enabled web server listening on port ',process.env.WEB_PORT);
+  })
+
+app.get('/', function (request, response, next) {
+    response.json({msg: 'This is CORS-enabled for all origins!'})
+})
 
 // MIDDLEWARE REGISTRATIONS
 // app.use(callback1, callback2, callback3)
@@ -43,8 +34,8 @@ app.use(session({
 
 
 // app.use(routeBase, callback);
-app.use("/project", require("./controllers/project.routes"));
-app.use("/equipement", require("./controllers/equipement.routes"));
-app.use("/client", require("./controllers/client.routes"));
-app.use("/staff", require("./controllers/staff.routes"));
+app.use("/toudoomapi/project", require("./controllers/project.routes"));
+app.use("/toudoomapi/equipement", require("./controllers/equipment.routes"));
+app.use("/toudoomapi/client", require("./controllers/client.routes"));
+app.use("/toudoomapi/staff", require("./controllers/staff.routes"));
 // app.use("/auth", require("./controllers/auth.route"));
