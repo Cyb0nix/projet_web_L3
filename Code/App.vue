9fbdd055222c0965355f2a705f23b12eb3ffda04
JulienS78 <julien.starck@efrei.net>
2022-12-07 01:26:29 +0100

    Adding Vue3 V0

---
+++ Code/App.vue
@@ -0,0 +1,182 @@
+
+<template>
+  <div id="app">
+    <img v-bind:src="require('./asset/Back.png')" alt="background" class="Back"/>
+    <div class="pagemenu">
+            <span class="txtProjects"><span>Projects</span></span>
+            <span class="txtStaffs"><span>Staffs</span></span>
+            <span class="txtEquipement"><span>Equipment</span></span>
+            <span class="txtMENU"><span>MENU</span></span>
+          </div>
+          <div class="menu">
+            <img
+            v-bind:src="require('./asset/image 1.png')"
+              alt="logo"
+              class="logo"
+            />
+          </div>
+          <div class="navTab">
+            <span class="txtVOD"><span>VOD</span></span>
+            <span class="txtEvent"><span>Event</span></span>
+            <span class="txtHome"><span>Home</span></span>
+            <img
+            v-bind:src="require('./asset/Ellipse 1.png')"
+              alt="pp"
+              class="pp"/>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style scoped>
+.body {
+  width: 95%;
+  height: 100%;
+}
+
+.Back {
+  width: 100%;
+  height: 721px;
+  color: #0C0923;
+}
+.pagemenu {
+  top: 11%;
+  left: 0px;
+  width: 230px;
+  height: 89%;
+  display: flex;
+  position: absolute;
+  border-radius: 0px 5px 0px 0px;
+  background-color: rgba(17, 12, 54, 1);
+}
+
+@media only screen and (max-width: 1300px){
+  .pagemenu{
+      display: none;
+  }
+}
+
+.menu {
+  top: 0px;
+  left: 0px;
+  width: 100%;
+  height: 75px;
+  display: flex;
+  position: absolute;
+  background-color: rgba(17, 12, 53, 0.95);
+}
+
+.logo {
+  top: 15%;
+  left: 1%;
+  width: 186px;
+  height: 53px;
+  position: absolute;
+  border-color: transparent;
+}
+
+@media only screen and (max-width: 600px){
+  .logo{
+      display: none;
+  }
+}
+
+.navTab {
+  top: 1.7%;
+  right: 1%;
+  height: 50px;
+  display: flex;
+  padding: 0;
+  position: absolute; 
+  background-color: transparent;
+}
+
+@media only screen and (max-width: 1500px){
+  .navTab{
+      top:2.7%;
+  }
+}
+
+.txtProjects {
+  top: 15%;
+  left: 6%;
+  color: rgba(255, 255, 255, 1);
+  position: absolute;
+  font-size: 20px;
+  font-family: NOMA;
+}
+.txtProjects :hover{
+  color: #D82367;
+}
+
+.txtStaffs {
+  top: 20%;
+  left: 6%;
+  color: rgba(255, 255, 255, 1);
+  position: absolute;
+  font-size: 20px;
+  font-family: NOMA;
+}
+
+.txtStaffs :hover{
+  color: #D82367;
+}
+.txtEquipement {
+  top: 25%;
+  left: 6%;
+  color: rgba(255, 255, 255, 1);
+  position: absolute;
+  font-size: 20px;
+  font-family: NOMA;
+}
+
+.txtEquipement :hover{
+  color: #D82367;
+}
+.txtMENU {
+  top: 5%;
+  left: 6%;
+  color: #D82367;
+  position: absolute;
+  font-size: 20px;
+  font-family: NOMA;
+}
+
+.txtMENU :hover{
+  color: #ffffff;
+}
+
+.txtVOD {
+  top: 21%;
+  left: 45%;
+  color: rgba(255, 255, 255, 1);
+  position: relative;
+  font-size: 24px;
+  font-family: NOMA;
+}
+
+.txtEvent {
+  top: 21%;
+  left: -15%;
+  color: rgba(255, 255, 255, 1);
+  position: relative;
+  font-size: 24px;
+  font-family: NOMA;
+}
+
+.txtHome {
+  top: 21%;
+  left: -85%;
+  color: #D82367;
+  position: relative;
+  font-size: 24px;
+  font-family: NOMA;  
+}
+
+</style>
