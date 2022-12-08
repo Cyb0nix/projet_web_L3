a62fdc1e53388e46ab6203423ba1021c432707d3
Cyb0nix <116296259+Cyb0nix@users.noreply.github.com>
2022-12-08 17:48:41 +0100

    Update SQL

--- Code/DB.sql
+++ Code/DB.sql
@@ -1,103 +1,118 @@
 create table clients
 (
-    Client_ID int auto_increment
+    clientId int auto_increment
         primary key,
-    Name      varchar(50) not null,
-    Number    varchar(20) not null,
-    Email     varchar(50) not null
+    name     varchar(50) not null,
+    number   varchar(20) not null,
+    email    varchar(50) not null
 );
 
-create table equipement
+create table equipment
 (
-    Equipement_ID int auto_increment
+    equipmentID  int auto_increment
         primary key,
-    Name          varchar(50)  not null,
-    `Condition`   varchar(255) not null,
-    Type          varchar(50)  not null,
-    Purchase_Date date         not null,
-    Available     tinyint(1)   not null,
-    Storage_Place varchar(20)  not null,
-    Renting_Rate  int          not null,
-    Bail_Rate     int          not null
+    name         varchar(50)  not null,
+    `condition`  varchar(255) not null,
+    type         varchar(50)  not null,
+    purchaseDate date         not null,
+    available    tinyint(1)   not null,
+    storagePlace varchar(20)  not null,
+    rentingRate  int          not null,
+    bailRate     int          not null
 );
 
 create table project
 (
-    Project_ID    int auto_increment
+    projectID    int auto_increment
         primary key,
-    Type          varchar(20) not null,
-    starting_date date        not null,
-    ending_date   date        not null,
-    Is_Paid       tinyint(1)  not null,
-    Benefits      int         not null,
-    state         varchar(50) not null,
-    Client        int         null,
+    type         varchar(20) not null,
+    startingDate date        not null,
+    endingDate   date        not null,
+    isPaid       tinyint(1)  not null,
+    benefits     int         not null,
+    state        varchar(50) not null,
+    client       int         not null,
+    projectName  int         null,
     constraint project_ibfk_1
-        foreign key (Client) references clients (Client_ID)
+        foreign key (client) references clients (clientId)
 );
 
-create table equipementused
+create table equipmentused
 (
-    EquipementUsed_ID int auto_increment primary key,
-    Equipement_ID  int          not null,
-    Project_ID     int          not null,
-    StartCondition varchar(255) not null,
-    EndCondition   varchar(255) not null,
-    constraint equipementused_ibfk_1
-        foreign key (Equipement_ID) references equipement (Equipement_ID),
-    constraint equipementused_ibfk_2
-        foreign key (Project_ID) references project (Project_ID)
+    equipmentUsedID int auto_increment
+        primary key,
+    equipmentID     int not null,
+    projectID       int not null,
+    constraint equipmentID
+        foreign key (equipmentID) references equipment (equipmentID)
+            on update cascade on delete cascade,
+    constraint projectID
+        foreign key (projectID) references project (projectID)
+            on update cascade on delete cascade
 );
-
-create index Project_ID
-    on equipementused (Project_ID);
 
 create index Client
-    on project (Client);
+    on project (client);
 
 create table skills
 (
-    Skill_ID int auto_increment
+    skillID int auto_increment
         primary key,
-    Skill    varchar(50) not null
+    skill   varchar(50) not null
 );
 
 create table staff
 (
-    Member_ID  int auto_increment
+    staffID   int auto_increment
         primary key,
-    Name       varchar(50) not null,
-    Discord_ID varchar(20) not null,
-    Email      varchar(50) not null,
-    Phone      varchar(20) not null,
-    Role       varchar(20) not null,
-    Join_Date  date        not null,
-    Is_formed  tinyint(1)  not null
+    name      varchar(50) not null,
+    discordID varchar(20) not null,
+    email     varchar(50) not null,
+    phone     varchar(20) not null,
+    role      varchar(20) not null,
+    joinDate  date        not null,
+    isFormed  tinyint(1)  not null,
+    mdp       varchar(20) not null,
+    isAdmin   tinyint(1)  not null
 );
 
 create table assigments
 (
-    Assigments_ID int auto_increment primary key,
-    Project_ID int         not null,
-    Member_ID  int         not null,
-    Task       varchar(50) not null,
-    constraint assigments_ibfk_1
-        foreign key (Project_ID) references project (Project_ID),
-    constraint assigments_ibfk_2
-        foreign key (Member_ID) references staff (Member_ID)
+    assigmentID int auto_increment
+        primary key,
+    staffID     int not null,
+    projectID   int not null,
+    task        int not null,
+    constraint assigments_project_null_fk
+        foreign key (projectID) references project (projectID)
+            on update cascade on delete cascade,
+    constraint assigments_staff_null_fk
+        foreign key (staffID) references staff (staffID)
+            on update cascade on delete cascade
 );
 
-create index Member_ID
-    on assigments (Member_ID);
-
-create table staffskill
+create table staffskills
 (
-    StaffSkill_ID int auto_increment primary key,
-    Skill_ID  int not null,
-    Member_ID int not null,
-    constraint Skill_ID
-        foreign key (Skill_ID) references skills (Skill_ID),
-    constraint Staff_ID
-        foreign key (Member_ID) references staff (Member_ID)
+    staffSkillsID int auto_increment
+        primary key,
+    skillID       int null,
+    staffID       int not null,
+    constraint staffskills_skills_null_fk
+        foreign key (skillID) references skills (skillID)
+            on update cascade on delete cascade,
+    constraint staffskills_staff_null_fk
+        foreign key (staffID) references staff (staffID)
+            on update cascade on delete cascade
+);
+
+create table users
+(
+    userID      int auto_increment
+        primary key,
+    username    varchar(20)  not null,
+    userEmail   varchar(40)  null,
+    userRole    varchar(10)  not null,
+    userPass    varchar(100) not null,
+    userCreated datetime     not null
 );
 
