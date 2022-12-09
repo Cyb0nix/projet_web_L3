b130eb2f17bbe0500ab1086a9e3e92200fbf1044
Cyb0nix <116296259+Cyb0nix@users.noreply.github.com>
2022-12-09 13:11:06 +0100

    Added Navbar component

--- Code/Vue/toudoom-front/src/views/HomeView.vue
+++ Code/Vue/toudoom-front/src/views/HomeView.vue
@@ -1,9 +1,34 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import NavbarVue from '../components/Navbar.vue';
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  
+
+  <div class="container-fluid" style="--bs-gutter-x: 0rem">
+    <NavbarVue/>
+    <div>
+      <img src="@/assets/image/bg_1.png" class="bg-image" alt="">
+    </div>
+    
+    <!-- <h1>Association de production vidéo de l'Efrei</h1> -->
+
+
+  </div>
 </template>
+
+<style>
+.bg-image{
+  position: relative;
+  width: 102%;
+  height: 816px;
+  top: -4px;
+  object-fit: contain;
+    
+    
+  
+  background: url(@/assets/image/bg_1.png);
+  filter: blur(10px);
+}
+
+</style>
