ae881c0a9f0efe0f3e34b7cc2e93926ca41d98e4
JulienS78 <julien.starck@efrei.net>
2022-12-13 00:19:49 +0100

    Add Grid for Equipment and Project Page

--- Code/Vue/toudoom-front/src/views/EquipmentPage.vue
+++ Code/Vue/toudoom-front/src/views/EquipmentPage.vue
@@ -3,7 +3,7 @@
 <template>
   <div style="background-color: #0C0923;">
     <div class="back">
-      <div class="row" style="--bs-gutter-x: 0">
+        <div class="row" style="--bs-gutter-x: 0">
           <div class="col-md-auto">
             <!-- SideBar -->
             <div class="d-flex flex-column flex-shrink-0 p-3" style="width: 280px; height: 100vh; background-color: #110c36">
@@ -15,7 +15,7 @@
                   ><img src="@/assets/image/logo.svg" alt="" width="150px"/>
                 </span>
               </a>
-              <hr/>
+              <hr />
               <ul class="nav nav-pills flex-column mb-auto">
                 <li class="nav-item" style="--bs-nav-pills-link-active-bg: #d82367">
                   <a href="#" class="nav-link active" aria-current="page">
@@ -65,265 +65,86 @@
             </div>
           </div>
           <div class="col-9">
-            <h2 class="p-3">Equipments</h2>
-            <div class="all-case-project">
-              <div class="case-equipement">
-                <span class="text14"><span>Project History</span></span>
-                <span class="text23">
-                  <span>
-                      Text..
-                  </span>
-                </span>
+
+            <div class="row">
+              <div class="col">
+                <h2 class="p-3" style="margin-left: 6%; margin-top: 5%;">Project</h2>
               </div>
-              <div class="case-information">
-                <span class="text18"><span>INFORMATION</span></span>
-                <span class="text19">
-                  <span>
-                    Text.
-                  </span>
-                </span>
+              <div class="col p-3">
+                <input type="button" class="btn btn-primary position-absolute top-1 end-0 add" style="margin-top: 1.7%; margin-right: 3.8%;" value="Edit">
               </div>
-              <button type="button" class="edit-button">
-                <span class="text20"><span>EDIT</span></span>
-                <img src="@/assets/image/Vector.svg" alt="Icon_edit" class="editimg" />
-              </button>
+            </div>
+
+            <div class="container" style="margin-right: -5%; margin-left:4%">
+              <table class="table table-striped table-hover" style="color: white">
+                <div class="row">
+                  <div class="col" style="background-color: rgb(11, 8, 36); width: 102%; height: 270px; border-radius: 15px 15px 15px 15px">
+                    <div class="col" style="font-family: NOMA">
+                      <div class="row">
+                        <div class="col" style="font-size: 20px; margin-bottom: 3%;">INFORMATION</div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col" style="background-color: rgb(11, 8, 36); width: 102%; height: 270px; border-radius: 15px 15px 15px 15px;margin-top: 3%; margin-left: -1%;">
+                  <div class="col" style="margin-left: 2%; margin-right:2%;font-family: NOMA">
+                    <div class="row">
+                      <div class="col" style="font-size: 20px">PROJECT HISTORY</div>
+                    </div>
+                      <table class="table table-striped table-hover" style="color: white;text-align: center">
+                        <thead>
+                          <tr>
+                            <th scope="col">ID</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Type</th>
+                            <th scope="col">StartDate</th>
+                            <th scope="col">EndDate</th>
+                            <th scope="col">Client</th>
+                            <th scope="col">State</th>
+                          </tr>
+                          <tr class="data" v-for="p of projects" v-bind:key="p.projectID" @click="openProject()">
+                            <td>{{ p.projectID }}</td>
+                            <td>{{ p.projectName }}</td>
+                            <td>{{ p.type }}</td>
+                            <td>{{ p.startingDate.split("T")[0]}}</td>
+                            <td>{{ p.endingDate.split("T")[0] }}</td>
+                            <td>{{ p.clientID }}</td>
+                            <td>{{ p.state }}</td>
+                          </tr>
+                        </thead>
+                      </table>
+                  </div>
+                </div>
+              </table>
             </div>
           </div>
-      </div>
+        </div>
     </div>
   </div>
 </template>
 
 <style scoped>
 
-.pagemenu {
-  left: 0px;
-  width: 230px;
-  height: 100vh;
-  display: flex;
-  position: absolute;
-  background-color: rgba(17, 12, 54, 1);
-}
-
-@media only screen and (max-width: 1300px) {
-  .pagemenu {
-    display: none;
-  }
-}
-
-
 .back {
   width: 100%;
   height: 721px;
   color: #0c0923;
 }
-
-.text {
-  top: 15%;
-  left: 6%;
-  color: #ffffff;
-  position: absolute;
-  font-size: 20px;
-  font-family: NOMA;
-}
-.text :hover {
-  color: #d82367;
+.data:hover {
+    color: #d82367;
+    background-color: rgba(255, 255, 255, 0.05);
 }
 
-.text02 {
-  top: 20%;
-  left: 6%;
-  color: rgba(255, 255, 255, 1);
-  position: absolute;
-  font-size: 20px;
-  font-family: NOMA;
+.add{
+    margin-right: 5%; 
+    margin-top: 0.6%; 
+    background-color: #d82367; 
+    border-color: #d82367;
 }
 
-.text02 :hover {
-  color: #d82367;
-}
-.text04 {
-  top: 25%;
-  left: 6%;
-  color: #d82367;
-  position: absolute;
-  font-size: 20px;
-  font-family: NOMA;
+.add:hover{
+    background-color: #d823686e;
+    border-color: #d823686e;
 }
 
-.text04 :hover {
-  color: #ffffff;
-}
-.text06 {
-  top: 5%;
-  left: 6%;
-  color: #d82367;
-  position: absolute;
-  font-size: 30px;
-  font-family: NOMA;
-}
-
-.text06 :hover {
-  color: #ffffff;
-}
-.all-case-project {
-  top: 106px;
-  left: 18%;
-  width: 63%;
-  height: 560px;
-  display: flex;
-  position: absolute;
-  border-radius: 0px 0px 0px 0px;
-  background-color: transparent;
-}
-
-@media only screen and (max-width: 1300px) {
-  .all-case-project {
-    margin-left: -8%;
-  }
-}
-
-.case-information {
-  top: 10%;
-  left: 0px;
-  width: 125%;
-  height: 216px;
-  display: flex;
-  position: absolute;
-  border-radius: 15px 15px 15px 15px;
-  background-color: rgba(17, 12, 54, 1);
-}
-
-.text18 {
-  top: 3%;
-  left: 3%;
-  color: rgb(255, 255, 255);
-  width: 100%;
-  height: auto;
-  position: absolute;
-  font-size: 20px;
-  font-family: NOMA;
-}
-
-.text18 :hover {
-  color: #d82367;
-}
-
-@media only screen and (max-width: 500px) {
-  .text18 {
-    left: 10.5%;
-  }
-}
-
-.text19 {
-  top: 20%;
-  left: 3%;
-  max-width: 500px;
-  max-height: 150px;
-  margin-right: 8%;
-  margin-left: 3%;
-  color: rgb(255, 255, 255);
-  position: relative;
-  font-size: 12px;
-  font-family: NOMA;
-  overflow: hidden;
-  display: -webkit-box;
-  -webkit-line-clamp: 15;
-  -webkit-box-orient: vertical;
-  line-height: 180%;
-}
-
-
-.text23 {
-  top: 20%;
-  left: 3%;
-  max-width: 1500px;
-  max-height: 192px;
-  margin-right: 6%;
-  margin-left: 0;
-  color: rgb(255, 255, 255);
-  position: relative;
-  font-size: 12px;
-  font-family: NOMA;
-  overflow: hidden;
-  display: -webkit-box;
-  -webkit-line-clamp: 15;
-  -webkit-box-orient: vertical;
-}
-
-.case-equipement {
-  top: 55%;
-  left: 0px;
-  width: 125%;
-  height: 260px;
-  display: flex;
-  position: absolute;
-  border-radius: 15px 15px 15px 15px;
-  background-color: rgba(17, 12, 54, 1);
-}
-
-.text14 {
-  top: 3%;
-  left: 1.5%;
-  color: rgb(255, 255, 255);
-  width: 100%;
-  height: auto;
-  position: absolute;
-  font-size: 20px;
-  font-family: NOMA;
-}
-
-.text14 :hover {
-  color: #d82367;
-}
-
-@media only screen and (max-width: 500px) {
-  .text14 {
-    left: 4%;
-  }
-}
-
-.text22 {
-  top: 0%;
-  color: rgb(255, 255, 255);
-  width: 15%;
-  position: absolute;
-  font-size: 32px;
-  font-family: NOMA;
-}
-
-.edit-button {
-  top: 1.5%;
-  right: -25%;
-  width: 90px;
-  height: 32px;
-  display: block;
-  position: absolute;
-  border-radius: 15px 15px 15px 15px;
-  background-color: rgba(17, 12, 54, 1);
-}
-
-.editimg {
-  top: 28%;
-  right: 17%;
-  width: 15%;
-  height: auto;
-  position: absolute;
-}
-
-.text20 {
-  top: 17%;
-  left: 19%;
-  color: rgba(255, 255, 255, 1);
-  width: 42px;
-  height: auto;
-  position: absolute;
-  font-size: 16px;
-  font-family: NOMA;
-  vertical-align: center;
-}
-
-.text20 :hover {
-  color: #d82367;
-}
 </style>
