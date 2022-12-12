3970d23558874002584dd4d35ece5878ba020a49
InspMaurice <94603445+InspMaurice@users.noreply.github.com>
2022-12-12 12:13:33 +0100

    Staff edit style

--- Code/Vue/toudoom-front/src/views/StaffEdit.vue
+++ Code/Vue/toudoom-front/src/views/StaffEdit.vue
@@ -38,3 +38,306 @@
     </div>
   </div>
 </template>
+
+<style scoped>
+
+.pagemenu {
+  left: 0px;
+  width: 230px;
+  height: 100vh;
+  display: flex;
+  position: absolute;
+  background-color: rgba(17, 12, 54, 1);
+}
+
+@media only screen and (max-width: 1300px) {
+  .pagemenu {
+    display: none;
+  }
+}
+
+.back {
+  width: 100%;
+  height: 721px;
+  color: #0c0923;
+}
+
+.text {
+  top: 15%;
+  left: 6%;
+  color: #d82367;
+  position: absolute;
+  font-size: 20px;
+  font-family: NOMA;
+}
+.text :hover {
+  color: #ffffff;
+}
+
+.text02 {
+  top: 20%;
+  left: 6%;
+  color: rgba(255, 255, 255, 1);
+  position: absolute;
+  font-size: 20px;
+  font-family: NOMA;
+}
+
+.text02 :hover {
+  color: #d82367;
+}
+.text04 {
+  top: 25%;
+  left: 6%;
+  color: rgba(255, 255, 255, 1);
+  position: absolute;
+  font-size: 20px;
+  font-family: NOMA;
+}
+
+.text04 :hover {
+  color: #d82367;
+}
+.text06 {
+  top: 5%;
+  left: 6%;
+  color: #d82367;
+  position: absolute;
+  font-size: 30px;
+  font-family: NOMA;
+}
+
+.text06 :hover {
+  color: #ffffff;
+}
+.all-case-staff {
+  top: 106px;
+  left: 18%;
+  width: 63%;
+  height: 560px;
+  display: flex;
+  position: absolute;
+  border-radius: 0px 0px 0px 0px;
+  background-color: transparent;
+}
+
+@media only screen and (max-width: 1300px) {
+  .all-case-staff {
+    margin-left: -8%;
+  }
+}
+
+.case-information {
+  top: 10%;
+  left: 0px;
+  width: 60%;
+  height: 216px;
+  display: flex;
+  position: absolute;
+  border-radius: 15px 15px 15px 15px;
+  background-color: rgba(17, 12, 54, 1);
+}
+
+.text18 {
+  top: 3%;
+  left: 3%;
+  color: rgb(255, 255, 255);
+  width: 100%;
+  height: auto;
+  position: absolute;
+  font-size: 20px;
+  font-family: NOMA;
+}
+
+.text18 :hover {
+  color: #d82367;
+}
+
+.text19 {
+  top: 20%;
+  left: 3%;
+  max-width: 500px;
+  max-height: 150px;
+  margin-right: 8%;
+  margin-left: 3%;
+  color: rgb(255, 255, 255);
+  position: relative;
+  font-size: 12px;
+  font-family: NOMA;
+  overflow: hidden;
+  display: -webkit-box;
+  -webkit-line-clamp: 15;
+  -webkit-box-orient: vertical;
+  line-height: 180%;
+}
+
+.case-skills {
+  top: 10%;
+  left: 65%;
+  width: 60%;
+  height: 216px;
+  display: flex;
+  padding: 0;
+  position: absolute;
+  border-radius: 15px 15px 15px 15px;
+  background-color: rgba(17, 12, 54, 1);
+}
+
+.frame {
+  top: 3%;
+  left: 2.5%;
+  width: 5%;
+  height: 25px;
+  position: absolute;
+}
+
+@media only screen and (max-width: 1300px) {
+  .frame {
+    display: none;
+  }
+}
+
+.text16 {
+  top: 3%;
+  left: 8%;
+  color: rgb(255, 255, 255);
+  width: 100%;
+  height: auto;
+  position: absolute;
+  font-size: 20px;
+  font-family: NOMA;
+}
+
+@media only screen and (max-width: 1300px) {
+  .text16 {
+    left: 5%;
+  }
+}
+
+@media only screen and (max-width: 500px) {
+  .text16 {
+    left: 8%;
+  }
+}
+
+.text16 :hover {
+  color: #d82367;
+}
+
+.text23 {
+  top: 20%;
+  left: 3%;
+  max-width: 1500px;
+  max-height: 192px;
+  margin-right: 6%;
+  margin-left: 0;
+  color: rgb(255, 255, 255);
+  position: relative;
+  font-size: 12px;
+  font-family: NOMA;
+  overflow: hidden;
+  display: -webkit-box;
+  -webkit-line-clamp: 15;
+  -webkit-box-orient: vertical;
+}
+
+.case-project {
+  top: 55%;
+  left: 0px;
+  width: 125%;
+  height: 260px;
+  display: flex;
+  position: absolute;
+  border-radius: 15px 15px 15px 15px;
+  background-color: rgba(17, 12, 54, 1);
+}
+
+.text14 {
+  top: 3%;
+  left: 1.5%;
+  color: rgb(255, 255, 255);
+  width: 100%;
+  height: auto;
+  position: absolute;
+  font-size: 20px;
+  font-family: NOMA;
+}
+
+.text14 :hover {
+  color: #d82367;
+}
+
+@media only screen and (max-width: 500px) {
+  .text14 {
+    left: 4%;
+  }
+}
+
+.text21 {
+  top: 20%;
+  left: 3%;
+  max-width: 500px;
+  max-height: 150px;
+  margin-right: 8%;
+  margin-left: 3%;
+  color: rgb(255, 255, 255);
+  position: relative;
+  font-size: 12px;
+  font-family: NOMA;
+  overflow: hidden;
+  display: -webkit-box;
+  -webkit-line-clamp: 15;
+  -webkit-box-orient: vertical;
+}
+
+.text22 {
+  top: 0%;
+  color: rgb(255, 255, 255);
+  width: 15%;
+  position: absolute;
+  font-size: 32px;
+  font-family: NOMA;
+}
+
+.save-button {
+  top: 1.5%;
+  right: -25%;
+  width: 90px;
+  height: 32px;
+  display: block;
+  position: absolute;
+  border-radius: 15px 15px 15px 15px;
+  background-color: rgba(17, 12, 54, 1);
+}
+
+.saveimg {
+  top: 23.5%;
+  right: 16%;
+  width: 15%;
+  height: auto;
+  position: absolute;
+}
+
+.editimg {
+  top: 5.5%;
+  right: 15px;
+  width: 15px;
+  height: auto;
+  position: absolute;
+}
+.text20 {
+  top: 17%;
+  left: 14%;
+  color: rgba(255, 255, 255, 1);
+  width: 42px;
+  height: auto;
+  position: absolute;
+  font-size: 16px;
+  font-family: NOMA;
+  vertical-align: center;
+}
+
+.text20 :hover {
+  color: #d82367;
+}
+</style>
