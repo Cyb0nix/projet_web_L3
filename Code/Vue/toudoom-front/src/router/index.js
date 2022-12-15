79049887c95f4d59a48bd28100ad09d416a592c6
Cyb0nix <116296259+Cyb0nix@users.noreply.github.com>
2022-12-15 04:18:49 +0100

    refactor

--- Code/Vue/toudoom-front/src/router/index.js
+++ Code/Vue/toudoom-front/src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import ProjectPage from '../views/ProjectpageView.vue'
+import ProjectPage from '../views/Projectpage.vue'
 import ProjectEdit from '../views/ProjectEdit.vue'
 import ProjectList from '../views/ProjectList.vue'
 import EquipmentPage from '../views/EquipmentPage.vue'
